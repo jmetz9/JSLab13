423eaca079c8baba7589a833c6e691ea11289039
jmetz9 <jmetz9@my.wctc.edu>
2021-05-11 11:40:42 -0500

    added sorting methods for the array

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
-    $("button").on("click", getGrade);
+    $("button#add").on("click", getGrade);
+    $("button#sortName").on("click", printByName);
+    $("button#sortPercent").on("click", printByPercent);
 });
+
+let studentList = [];
 
 function getGrade(){
 
     let pointsEarned = $("input#pointsEarned").val()
     let pointsPossible = $("input#pointsPossible").val()
     let percent = ((pointsEarned/pointsPossible)*100).toFixed(2)
+
 
     let letter = "A"
     if(percent < 90){
@@ -23,15 +28,68 @@ function getGrade(){
     }
 
     let student = {
-        fname: $("input#fname").val(),
-        lname: $("input#lname").val(),
+        firstName: $("input#fname").val(),
+        lastName: $("input#lname").val(),
         percentage: percent,
         grade: letter
     }
 
-    $("p#fnameOutput").text("First name: " + student.fname)
-    $("p#lnameOutput").text("Last name: " + student.lname)
+    $("p#fnameOutput").text("First name: " + student.firstName)
+    $("p#lnameOutput").text("Last name: " + student.lastName)
     $("p#percentOutput").text("Score: " + student.percentage + "%")
     $("p#gradeOutput").text("Letter grade: " + student.grade)
+
+    studentList.push(JSON.stringify(student))
+    $("p#listOutput").append(JSON.stringify(student))
+    $("p#listOutput").append("\n")
+}
+
+function printByName(){
+    $("p#listOutput").text("");
+    studentList.sort(sortName);
+
+    for(let i = 0; i < studentList.length; i++){
+        $("p#listOutput").append(JSON.stringify(studentList[i]) + "\n")
+    }
+}
+
+function printByPercent(){
+    $("p#listOutput").text("");
+    studentList.sort(sortPercent);
+
+    for(let i = 0; i < studentList.length; i++){
+        $("p#listOutput").append(JSON.stringify(studentList[i]) + "\n")
+    }
+}
+
+function sortName(a,b){
+
+    if(a.firstName < b.firstName){
+        return -1
+    }
+
+    else if(a.firstName > b.firstName){
+        return 1
+    }
+
+    else{
+        return 0
+    }
+
+}
+
+function sortPercent(a,b){
+
+    if(a.percentage < b.percentage){
+        return -1
+    }
+
+    else if(a.percentage > b.percentage){
+        return 1
+    }
+
+    else{
+        return 0
+    }
 
 }
